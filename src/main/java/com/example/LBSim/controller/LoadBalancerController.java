4035849aeed0d918291d0201b4a2b4bdf2dc6cec
mohamedbadawi7 <mohamed.badawi@usask.ca>
2025-05-15 20:03:11 +0300

    Added server logic

--- src/main/java/com/example/LBSim/controller/LoadBalancerController.java
+++ src/main/java/com/example/LBSim/controller/LoadBalancerController.java
@@ -11,7 +11,22 @@ public class LoadBalancerController {
 
     private List<Server> servers;
 
-    public LoadBalancerController(List<Server> servers) {
+    public LoadBalancerController() {
         this.servers = new ArrayList<>();
+        Server s = new Server(1);
+        this.servers.add(s);
+    }
+
+    private int numServers() {
+        return servers.size();
+    }
+
+    private void addServer() {
+        Server s = new Server(numServers()+1);
+        this.servers.add(s);
+    }
+
+    private void removeServer(Server s) {
+        this.servers.remove(s);
     }
 }
--- src/main/java/com/example/LBSim/entity/Server.java
+++ src/main/java/com/example/LBSim/entity/Server.java
@@ -1,13 +1,22 @@
 package com.example.LBSim.entity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.context.annotation.Primary;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Entity
 public class Server {
-    
+
+    @Id
     private int id;
+
     private int capacity;
     private int currentLoad;
     private boolean isHealthy;
+
+    public Server() {}
 
     public Server(int id) {
         this.id = id;
@@ -16,20 +25,40 @@ public class Server {
         this.isHealthy = true;
     }
 
-    public int getCapacity() {
+    private int getCapacity() {
         return capacity;
     }
 
-    public void incrementLoad() {
+    private void incrementLoad() {
         currentLoad++;
     }
 
-    public void decrementLoad() {
+    private void decrementLoad() {
         currentLoad--;
     }
 
-    public boolean atCapacity() {
+    private boolean atCapacity() {
         return currentLoad >= capacity;
+    }
+
+    public int handleRequest() {
+
+        if (isHealthy && !atCapacity()) {
+            incrementLoad();
+
+            CompletableFuture.runAsync(() -> {
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(500, 5000));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    decrementLoad();
+                }
+            });
+            return 1;
+        } else {
+            return -1;
+        }
     }
 
 
